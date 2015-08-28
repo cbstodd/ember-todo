3c74645a51193df07d6d7dfe384b63ec41ff87a1
Colin Stodd <cbstodd@gmail.com>
2015-08-27 21:48:08 -0400

    added todo components, model data, and routes to toggle as completed

--- app/models/todo.js
+++ app/models/todo.js
@@ -1,8 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  title: DS.attr('string'),
-  isCompleted: DS.attr('boolean')
+  title: DS.attr('string'),       // Sets title to a string variable
+  isCompleted: DS.attr('boolean') // Sets isCompleted to true or false
 
 }).reopenClass({
   FIXTURES: [
@@ -20,6 +20,16 @@ export default DS.Model.extend({
       id: 3,
       title: "Re-do Michael Hartl's Rails tutorial and create a project creation social app.",
       isCompleted: false
+    },
+    {
+      id: 4,
+      title: "Learn ember even more.",
+      isCompleted: false
+    },
+    {
+      id: 5,
+      title: "Start learning iOS development.",
+      isCompleted: true
     }
   ]
 });
